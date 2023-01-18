6847865e49a4f95916249721839803bb1992decd
Jamie Taber-Plaisted <jamietp113@gmail.com>
2023-01-17 22:02:19 -0500

    comments

--- Project/hello.cpp
+++ Project/hello.cpp
@@ -1,8 +1,17 @@
+/*
+ * hello.cpp
+ * prints out my name, state, and a hobby of mine
+ * by Jamie
+ */
+
 #include <cstdio>
 
 int main()
 {
+    // Print out my name
     printf("Hello, my name is Jamie.\n");
+    // Print out where i live
     printf("I live in New York.\n");
+    // Print out my nhobby
     printf("I like to snowboard");
 }
