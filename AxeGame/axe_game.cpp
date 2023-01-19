fdb7bbc307dd65366a702748483697539aff23a8
Jamie Taber-Plaisted <jamietp113@gmail.com>
2023-01-18 20:05:53 -0500

    debugging, comparison operators, and loops

--- AxeGame/axe_game.cpp
+++ AxeGame/axe_game.cpp
@@ -8,14 +8,30 @@ int main()
     int length;
     length = 200;
 
-    float root_beer;
-    root_beer = 1.99;
+    /* // debugging variables
+    float root_beer = 1.99;
 
-    double cheese_burger;
-    cheese_burger = 5.99;
+    double cheese_burger{5.99};
 
-    bool shouldHaveLunch;
-    shouldHaveLunch = true;
+    bool shouldHaveLunch{};
+
+    // comparison operators
+    // false
+    bool equal{4 == 9};
+    // true
+    bool notEqual{4 != 9};
+    // true
+    bool less{4 < 9};
+    // false
+    bool greater{4 > 9};
+    // true
+    bool lessEqual{4 <= 9};
+    // false
+    bool greaterEqual{4 >= 9}; */
 
     InitWindow(width, length, "Jamie's Window");
+
+    while (true)
+    {
+    }
 }
