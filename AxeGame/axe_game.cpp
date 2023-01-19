21f50d6048c2340849c0a1b355cf26fbdbfc29cf
Jamie Taber-Plaisted <jamietp113@gmail.com>
2023-01-18 19:42:00 -0500

    Data Types and Variables

--- AxeGame/axe_game.cpp
+++ AxeGame/axe_game.cpp
@@ -2,5 +2,20 @@
 
 int main()
 {
-    InitWindow(350, 200, "Jamie's Window");
+    int width;
+    width = 350;
+
+    int length;
+    length = 200;
+
+    float root_beer;
+    root_beer = 1.99;
+
+    double cheese_burger;
+    cheese_burger = 5.99;
+
+    bool shouldHaveLunch;
+    shouldHaveLunch = true;
+
+    InitWindow(width, length, "Jamie's Window");
 }
