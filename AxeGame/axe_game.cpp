1de96ef09507aebea7f2a9bfd1821e42173bc83f
Jamie Taber-Plaisted <jamietp113@gmail.com>
2023-01-20 20:35:01 -0500

    made circle and gave it movement

--- AxeGame/axe_game.cpp
+++ AxeGame/axe_game.cpp
@@ -2,12 +2,6 @@
 
 int main()
 {
-    int width;
-    width = 350;
-
-    int length;
-    length = 200;
-
     /* // debugging variables
     float root_beer = 1.99;
 
@@ -29,12 +23,37 @@ int main()
     // false
     bool greaterEqual{4 >= 9}; */
 
-    InitWindow(width, length, "Jamie's Window");
+    // window dimensions
+    int width{350};
+    int length{200};
+    InitWindow(width, length, "Axe Game");
+
+    // circle coordinates
+    int circle_x{175};
+    int circle_y{100};
+
+    SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RED);
-        EndDrawing();
+        ClearBackground(WHITE);
+
+        // Game logic begins
+
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+
+        if (IsKeyDown(KEY_D))
+        {
+            circle_x = circle_x + 10;
         }
+
+        if (IsKeyDown(KEY_A))
+        {
+            circle_x = circle_x - 10;
+        }
+
+        // Game logic ends
+        EndDrawing();
+    }
 }
