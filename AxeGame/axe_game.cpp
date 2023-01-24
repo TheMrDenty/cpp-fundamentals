4a55c96caaf9bc81d6715f5a3ec617ce7b6b34de
Jamie Taber-Plaisted <jamietp113@gmail.com>
2023-01-23 19:16:55 -0500

    axe game finished

--- AxeGame/axe_game.cpp
+++ AxeGame/axe_game.cpp
@@ -31,12 +31,33 @@ int main()
     // circle coordinates
     int circle_x{175};
     int circle_y{100};
+    int cicrle_radius{25};
+
+    // circle edges
+    int l_circle_x{circle_x - cicrle_radius};
+    int r_circle_x{circle_x + cicrle_radius};
+    int u_circle_y{circle_y - cicrle_radius};
+    int b_circle_y{circle_y + cicrle_radius};
 
     // axe coordinates
     int axe_x{400};
     int axe_y{0};
+    int axe_length{50};
+
+    // axe edges
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x + axe_length};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y + axe_length};
 
     int direction{10};
+
+    // collision detection
+    bool collision_with_axe =
+        (b_axe_y >= u_circle_y) &&
+        (u_axe_y <= b_circle_y) &&
+        (l_axe_x <= r_circle_x) &&
+        (r_axe_x >= l_circle_x);
 
     SetTargetFPS(60);
 
@@ -45,31 +66,58 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
-        // Game logic begins
-
-        DrawCircle(circle_x, circle_y, 25, BLUE);
-
-        DrawRectangle(axe_x, axe_y, 50, 50, RED);
-
-        // moves the axe
-        axe_y += direction;
-        if (axe_y > 450 || axe_y < 0)
+        if (collision_with_axe)
         {
-            direction = -direction;
+            DrawText("Game Over!", 400, 225, 20, RED);
         }
-
-        // moves the circle
-        if (IsKeyDown(KEY_D) && circle_x < 800)
+        else
         {
-            circle_x += 10;
-        }
+            // Game logic begins
 
-        if (IsKeyDown(KEY_A) && circle_x > 0)
-        {
-            circle_x -= 10;
-        }
+            // update the edges
+            // circle
+            l_circle_x = circle_x - cicrle_radius;
+            r_circle_x = circle_x + cicrle_radius;
+            u_circle_y = circle_y - cicrle_radius;
+            b_circle_y = circle_y + cicrle_radius;
 
-        // Game logic ends
+            // axe
+            l_axe_x = axe_x;
+            r_axe_x = axe_x + axe_length;
+            u_axe_y = axe_y;
+            b_axe_y = axe_y + axe_length;
+
+            // update collision with axe
+            collision_with_axe =
+                (b_axe_y >= u_circle_y) &&
+                (u_axe_y <= b_circle_y) &&
+                (l_axe_x <= r_circle_x) &&
+                (r_axe_x >= l_circle_x);
+
+            DrawCircle(circle_x, circle_y, cicrle_radius, BLUE);
+
+            DrawRectangle(axe_x, axe_y, 50, axe_length, RED);
+
+            // moves the axe
+            axe_y += direction;
+            if (axe_y > length || axe_y < 0)
+            {
+                direction = -direction;
+            }
+
+            // moves the circle
+            if (IsKeyDown(KEY_D) && circle_x < width)
+            {
+                circle_x += 10;
+            }
+
+            if (IsKeyDown(KEY_A) && circle_x > 0)
+            {
+                circle_x -= 10;
+            }
+
+            // Game logic ends
+        }
         EndDrawing();
     }
 }
